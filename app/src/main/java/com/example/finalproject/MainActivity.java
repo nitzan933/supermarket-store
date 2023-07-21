90e09eb7e704571039e0a4d773393c697c6fb4f3
nitzan <nitzan963@gmail.com>
2023-07-21 11:53:58 +0300

    settings

--- app/src/main/java/com/example/finalproject/MainActivity.java
+++ app/src/main/java/com/example/finalproject/MainActivity.java
@@ -1,17 +1,35 @@
 package com.example.finalproject;
 
-import androidx.appcompat.app.AppCompatActivity;
+import static android.content.ContentValues.TAG;
 
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.fragment.app.Fragment;
+import androidx.preference.ListPreference;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.PreferenceManager;
+
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.content.res.Configuration;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.Window;
 
 public class MainActivity extends AppCompatActivity implements Frag.OnClickListenerFrag {
 
+    private static Context context;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        context = getApplicationContext();
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+        changeTheme(prefs.getBoolean("switch_dark_theme", false));
         DetailsFragment fragB = (DetailsFragment) getSupportFragmentManager().findFragmentByTag("FRAGB");
         if ((getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE)) {
             if (fragB != null) {
@@ -20,7 +38,7 @@ public class MainActivity extends AppCompatActivity implements Frag.OnClickListe
                         .commit();
             } else {
                 getSupportFragmentManager().beginTransaction()
-                        .add(R.id.fragmentDetailContainerView, DetailsFragment.class, null, "FRAGB")
+                        .add(R.id.fragmentDetailContainerView2, DetailsFragment.class, null, "FRAGB")
                         .commit();
             }
             getSupportFragmentManager().executePendingTransactions();
@@ -33,10 +51,66 @@ public class MainActivity extends AppCompatActivity implements Frag.OnClickListe
         {
             getSupportFragmentManager().beginTransaction()
                     .setReorderingAllowed(true)
-                    .add(R.id.fragA, DetailsFragment.class, null,"FRAGB")
+                    .add(R.id.fragmentContainerView, DetailsFragment.class, null,"FRAGB")
                     .addToBackStack("BBB")
                     .commit();
             getSupportFragmentManager().executePendingTransactions();
         }
+    }
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu( Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.settings:
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.fragmentContainerView, new mySettings())
+                        .addToBackStack(null)
+                        .commit();
+                getSupportFragmentManager().executePendingTransactions();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public static class mySettings extends PreferenceFragmentCompat{
+
+        @Override
+        public void onCreatePreferences(@Nullable Bundle savedInstanceState, @Nullable String rootKey) {
+            setPreferencesFromResource(R.xml.settings, rootKey);
+            SharedPreferences.OnSharedPreferenceChangeListener listener;
+            //Loads Shared preferences
+            SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+            //Setup a shared preference listener for hpwAddress and restart transport
+            prefs.registerOnSharedPreferenceChangeListener(new SharedPreferences.OnSharedPreferenceChangeListener() {
+                public void onSharedPreferenceChanged(SharedPreferences prefs, String key) {
+                    if (key.equals("switch_dark_theme")) {
+                        changeTheme(prefs.getBoolean(key, false));
+                    }
+                }
+            });
+        }
+    }
+
+
+
+    public static void changeTheme(boolean dark){
+        if(dark)
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+        else AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+        super.onPointerCaptureChanged(hasCapture);
     }
 }
